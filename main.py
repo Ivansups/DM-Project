ab52c73f2c4da6d312b77826b4630969cee16eca
Ivanspus <klim2006.06@mail.ru>
2025-04-01 21:45:23 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from tkinter.filedialog import FileDialog
-from itertools import *
-
 def is_reflection(A, R): # Рефлексивность
     for x in A:
         if (x, x) not in R:
@@ -61,47 +58,3 @@ def make_simmetric(A):
         answ.append((A[x - 1], A[x]))
         answ.append((A[x], A[x - 1]))
     return answ
-
-Aaa = [1, 2, 3, 4]
-
-print(Aaa)
-print(is_trasition(Aaa, make_reflection(Aaa)))
-print(f'gol {make_simmetric(Aaa)}')
-print(f'gol {make_trasition(Aaa)}')
-
-
-A = [1, 2, 3]
-R1 = [(1, 1), (2, 2), (3, 3)] # Для двух видов рефлексивности
-R2 = [(1, 2), (2, 1)] # Для симметричности
-R3 = [(1, 1), (2, 2)] # Для асимметричности
-
-print(f'da {is_reflection(A, R1)}')
-print(f'da {is_simmetric(A, R1)}')
-print(f'da {is_trasition(A, R1)}')
-
-AT = [1, 2, 3]
-RT = [(1, 2), (1, 3), (3, 3), (2, 3), (2, 2)]
-
-e = ["a", "b", "c", "d", "e"]
-d2 = [('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e')]
-d3 = [('a', 'b'), ('b', 'c'), ('a', 'c'), ('b', 'd'), ('c', 'd'), ('d', 'e'), ('c', 'e'), ('a', 'e'), ('b', 'e'), ('a', 'd')]
-d4 = []
-
-print(f'Это тест рефлексивности ---> {is_reflection(A, R1)}')
-print()
-print(f'Это тест симметричности ---> {is_simmetric(A, R2)}')
-print()
-print(f'Это тест аисметричности ---> {is_asimetric(R3)}')
-print()
-print(f'Это тест транзитивности ---> {is_trasition(AT, RT)}')
-print()
-print(f't1 {is_simmetric(e, d4)}')
-print(f't2 {is_trasition(e, d4)}')
-print(f't3 {is_reflection(e, d4)}')
-
-# print(is_reflection(e, d))
-# print(is_simmetric(e, d))
-# print(is_asimetric(d))
-# print(is_trasition(e, d))
-
-#Диаграмма Хассе
